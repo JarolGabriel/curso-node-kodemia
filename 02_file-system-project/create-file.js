0d4b9b92df64d08b242c00f4b07b8e0b1b4f94d1
jarolGabriel <jarolgabriel.jimenez70@gmail.com>
2024-07-22 22:22:52 -0500

    new

--- 02_file-system-project/create-file.js
+++ 02_file-system-project/create-file.js
@@ -1,4 +1,8 @@
 //const fs = require("node:fs");
 const fs = require("fs");
 
-fs.writeFileSync("nuevo_archivo.txt", "xxxxxxxxxx", "utf-8");
+fs.writeFileSync(
+  "nuevo_archivo.txt",
+  "este es un ejemplo de como leer un archivo con readFile",
+  "utf-8"
+);
